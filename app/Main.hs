89465ca51560c747331e93645b729677441e196d
Matias <matias.castro-guzman@epitech.eu>
2020-03-24 18:52:40 +0100

    [ADD] Error messages

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,48 @@
 
 import System.Environment
 import System.Exit
+import System.IO
 
 main :: IO()
 main = getArgs >>= parse
 
-parse [] = usage >> exit
+parse ["--usage"] = usage >> exit
 
 parse [expression] = operate(expression) >> exit
 
-parse args = usage >> failure
+parse args = usage >> failure "Error: Invalid Arg"
 
 operate (expression) = do
-    if isAllowedChar(head expression) == True
-        then failure
-        else if (length expression > 1)
-            then operate(tail expression)
-            else calculate
+    if (length expression >= 1)
+        then do
+            stack(head expression)
+            operate(tail expression)
+    else calculate
+
+
+stack (c) = do
+    if (c >= '0' && c <= '9') || c == '.'
+        then print "number"
+    else if c == '('
+        then print "left par"
+    else if c == ')'
+        then print "right par"
+    else if isOperator(c) == True
+        then do
+            print "operator"
+    else do
+        failure "Error: Non-allowed character found" 
 
 calculate = do
-    print("OK")
-
-isAllowedChar :: Char -> Bool
-isAllowedChar (c) = do
-    if (c < '0' || c > '9') && isOperator(c) == False
-        then True
-        else False
+    print("Calculate")
 
 isOperator :: Char -> Bool
-isOperator (c) = c `elem` ".+-*/()"
+isOperator (c) = c `elem` ".+-*/"
 
 usage = do
     putStrLn "USAGE: ./funEvalExpr expression\n"
     putStrLn "      expression        the expression to calculate"            
 
 exit = exitWith ExitSuccess
-failure = exitWith (ExitFailure 84)
+failure :: String -> IO a
+failure str = hPutStrLn stderr str >> exitWith (ExitFailure 84)
