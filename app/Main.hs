633f1bf71e30534dc18194479722e6b9b93622ac
Matias <matias.castro-guzman@epitech.eu>
2020-03-24 13:40:01 +0100

    [ADD] Basic Error Handling
    - Iterates through string and check if characters are allowed

--- app/Main.hs
+++ app/Main.hs
@@ -12,16 +12,23 @@ parse [expression] = operate(expression) >> exit
 parse args = usage >> failure
 
 operate (expression) = do
-    putStrLn "OK"
+    if isAllowedChar(head expression) == True
+        then failure
+        else if (length expression > 1)
+            then operate(tail expression)
+            else calculate
+
+calculate = do
+    print("OK")
 
 isAllowedChar :: Char -> Bool
-isAllowedChar c = do
+isAllowedChar (c) = do
     if (c < '0' || c > '9') && isOperator(c) == False
         then True
         else False
 
 isOperator :: Char -> Bool
-isOperator c = c `elem` ".+-*/()"
+isOperator (c) = c `elem` ".+-*/()"
 
 usage = do
     putStrLn "USAGE: ./funEvalExpr expression\n"
