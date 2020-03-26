d96e89f604c27ac77ba43bb32a0779cb4795e6cc
Matias <matias.castro-guzman@epitech.eu>
2020-03-26 16:00:14 +0100

    [ADD] Further rpn
    - Store stack and output in rpn type
    - Can now handle parenthesis
    - Needs polishing (unintended pun) on operator handling

--- src/EvalExpr.hs
+++ src/EvalExpr.hs
@@ -8,45 +8,68 @@ import Text.Read
 evalExpr :: IO()
 evalExpr = getArgs >>= parse
 
+data Rpn = Rpn {output :: String, stack :: String}
+
 parse ["--usage"] = usage >> exit
 
 parse [expression] = do
+    let rpn = Rpn "" ""
+
     if getNumber(expression) == Nothing
-        then evaluate(expression)
+        then evaluate expression rpn '0'
     else putStrLn expression
     exit
 
 parse args = usage >> failure "Error: Invalid Arg"
 
-evaluate (expression) = do
+evaluate:: String -> Rpn -> Char -> IO()
+evaluate expression rpn lastc = do
+
     if (length expression >= 1)
         then do
-            stack(head expression)
-            evaluate(tail expression)
-    else calculate
+            let operated = operate (head expression) lastc rpn
+            if output operated == "Error"
+                then failure "Error: Non-allowed character found"
+            else
+                evaluate(tail expression) operated (head expression)
+    else calculate rpn
 
-
-stack (c) = do
+operate :: Char -> Char -> Rpn -> Rpn
+operate c lastc rpn = do
     if (c >= '0' && c <= '9') || c == '.'
-        then print "number"
+        then do
+            if (lastc >= '0' && lastc <= '9') || lastc == '.'
+                then Rpn (output rpn ++ charToString c) (stack rpn)
+            else Rpn (output rpn ++ " " ++ charToString c) (stack rpn)
     else if c == '('
-        then print "left par"
+        then Rpn (output rpn) (stack rpn ++ charToString c)
     else if c == ')'
-        then print "right par"
+        then rightParenthesisAction rpn
     else if isOperator(c) == True
         then do
-            print "operator"
-    else do
-        failure "Error: Non-allowed character found" 
+             Rpn (output rpn) (stack rpn ++ charToString c)
+    else Rpn "Error" "Error"
 
-calculate = do
-    print("Calculate")
+rightParenthesisAction :: Rpn -> Rpn
+rightParenthesisAction rpn = do
+    if length (stack rpn) >= 1 && head (stack rpn) /= '('
+        then do
+            let new = Rpn (output rpn ++ " " ++ charToString (head (stack rpn))) (tail (stack rpn))
+            rightParenthesisAction new
+    else Rpn (output rpn) (tail (stack rpn))
+
+calculate rpn = do
+    print(output rpn)
+    print(stack rpn)
+
+charToString :: Char -> String
+charToString c = [c]
 
 getNumber :: String -> Maybe Double
 getNumber str = readMaybe str :: Maybe Double
 
 isOperator :: Char -> Bool
-isOperator (c) = c `elem` ".+-*/"
+isOperator c = c `elem` ".+-*/^()"
 
 usage = do
     putStrLn "USAGE: ./funEvalExpr expression\n"
---
+++ src/Operations.hs
@@ -0,0 +1,1 @@
+module Operations where
